1a794079c0757f12c7bc629d5b905f04630071f7
melloware <mellowaredev@gmail.com>
2024-01-25 11:33:39 -0500

    Fixed typescript issues

--- src/notifier.ts
+++ src/notifier.ts
@@ -1,30 +1,30 @@
 import * as vscode from "vscode";
 
 class Notifier {
-    public statusBarItem: vscode.StatusBarItem;
-    private timeoutId: NodeJS.Timer | null;
+  public statusBarItem: vscode.StatusBarItem;
+  private timeoutId: NodeJS.Timeout | string | number | undefined;
 
-    constructor(command?: string, alignment?: vscode.StatusBarAlignment, priority?: number) {
-        this.statusBarItem = vscode.window.createStatusBarItem(alignment, priority);
-        this.statusBarItem.command = command;
-        this.statusBarItem.show();
-        this.timeoutId = null;
+  constructor(command?: string, alignment?: vscode.StatusBarAlignment, priority?: number) {
+    this.statusBarItem = vscode.window.createStatusBarItem(alignment, priority);
+    this.statusBarItem.command = command;
+    this.statusBarItem.show();
+    this.timeoutId = undefined;
+  }
+
+  public notify(icon: string, text: string, autoHide = true): void {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
 
-    public notify(icon: string, text: string, autoHide = true): void {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
+    this.statusBarItem.text = `$(${icon}) ${text}`;
+    this.statusBarItem.tooltip = undefined;
 
-        this.statusBarItem.text = `$(${icon}) ${text}`;
-        this.statusBarItem.tooltip = undefined;
-
-        if (autoHide) {
-            this.timeoutId = setTimeout(() => {
-                this.statusBarItem.text = `$(${icon})`;
-                this.statusBarItem.tooltip = text;
-            }, 5000);
-        }
+    if (autoHide) {
+      this.timeoutId = setTimeout(() => {
+        this.statusBarItem.text = `$(${icon})`;
+        this.statusBarItem.tooltip = text;
+      }, 5000);
     }
+  }
 }
 export default Notifier;
