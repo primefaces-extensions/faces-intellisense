cbbf24e9d929f20cb382d9349b6d4cc80c8e7914
Melloware <mellowaredev@gmail.com>
2024-08-06 09:28:29 -0400

    Fix #24 add HoverProvider (#25)
    * Fix #24 add Hoverprovider
    
    * Fix #24 add Hoverprovider
    
    * Add prettier confs
    
    * update builds
    
    * Formatting, test output, initial unit tests...
    
    * Hover works
    
    * Remove self dependency from build files
    
    ---------
    
    Co-authored-by: FlipWarthog <flipwarthog@gmail.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,23 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as vscode from "vscode";
+import * as myExtension from "../../extension";
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Extension Test Suite", () => {
+  suiteSetup(() => {
+    vscode.window.showInformationMessage("Starting tests!");
+  });
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  suiteTeardown(() => {
+    vscode.window.showInformationMessage("All tests done!");
+  });
+
+  test("inAttribute returns true for attribute", () => {
+    assert.equal(myExtension.inAttribute('value="'), true);
+  });
+  test("inAttribute returns false for element", () => {
+    assert.equal(myExtension.inAttribute("p:inputText"), false);
+  });
 });
